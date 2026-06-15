61cd449ee816683f068bcb7d20cc5be268e437e2
Pasquale <82742942+PasqualePerilli@users.noreply.github.com>
2026-06-15 09:29:02 -0500

    Update ask-credentials.bash

--- ask-credentials.bash
+++ ask-credentials.bash
@@ -65,9 +65,12 @@ _should_use_curl(){
 _manage_remote_downloads(){
   local useCurl=$(_should_use_curl)
   local filesToDownload=("https://raw.githubusercontent.com/PasqualePerilli/simplex-duckdns-setup/refs/heads/master/ask-credentials.bash" "https://raw.githubusercontent.com/PasqualePerilli/simplex-duckdns-setup/refs/heads/master/compose.yml" )
+  local downloadIndex=0
+  local numberOfFilesToDownload="${#filesToDownload[@]}"
   for fileToDownload in "${filesToDownload[@]}"; do
     local filename="${fileToDownload##*/}"
-    echo "Downloading file with name: $filename"
+	downloadIndex=$(( 1 + $downloadIndex ))
+    echo "Downloading file #${downloadIndex}/${numberOfFilesToDownload} with name: $filename"
     if [[ "$useCurl" == "true" ]]; then
 	  curl -s -k -H "Cache-Control: no-cache" -H "Pragma: no-cache" -o "$filename" "$fileToDownload" 	 #Use curl
     else
